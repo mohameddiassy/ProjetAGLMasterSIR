c15fe2d8d660fb388e6ea227c59bbad8d944dbfe
Hama10 <mballos1005@gmail.com>
2020-11-22 03:02:27 +0000

    FIRST COMMIT MBALLO...

---
+++ src/main/java/sn/groupeCana/projetAGL/Calculator.java
@@ -0,0 +1,54 @@
+package sn.groupeCana.projetAGL;
+
+/**
+ * Class Calculator!
+ *
+ */
+public class Calculator {
+    public int sum( int a, int b ) {
+
+        return 0;
+    }
+
+    public int minus( int a, int b ) {
+
+        return 0;
+    }
+
+    public int divide( int a, int b ) {
+
+        return 0;
+    }
+
+    public int multiply( int a, int b ) {
+
+        return 0;
+    }
+
+    public int min( int a, int b ) {
+
+        return 0;
+    }
+
+    public int max( int a, int b ) {
+
+        return 0;
+    }
+
+    /*
+     * Retourne le plus petit élement d'une liste
+     */
+    public int minElement( int[] list ) {
+
+        return 0;
+    }
+
+    /*
+     * Retourne le plus grand élement d'une liste
+     */
+    public int maxElement( int[] list ) {
+
+        return 0;
+    }
+
+}
